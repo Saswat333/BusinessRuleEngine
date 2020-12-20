fc43cee3575631ff87e948d74fde23236e78f725
Saswat333 <saswatsenapati333@gmail.com>
2020-12-20 23:59:17 +0530

    Initial Commit: Default springboot file. No code change

---
+++ src/main/java/com/BusinessRuleEngine/BusinessRuleEngineProject/BusinessRuleEngineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.BusinessRuleEngine.BusinessRuleEngineProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessRuleEngineProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessRuleEngineProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BusinessRuleEngine/BusinessRuleEngineProject/BusinessRuleEngineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BusinessRuleEngine.BusinessRuleEngineProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessRuleEngineProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
