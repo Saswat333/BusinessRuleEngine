0e4411bf5188280a3968eb92e916fda03c116a95
Saswat333 <saswatsenapati333@gmail.com>
2020-12-21 00:10:34 +0530

    Initial Test file commit. Check test fails

---
+++ src/main/java/com/BusinessRuleEngine/BusinessRuleEngineProject/OrderProcessService/OrderProcessFactoryService.java
@@ -0,0 +1,9 @@
+package com.BusinessRuleEngine.BusinessRuleEngineProject.OrderProcessService;
+
+public class OrderProcessFactoryService {
+
+
+    public String processOrder(int payment, int orderId) {
+        return null;
+    }
+}
---
+++ src/test/java/com/BusinessRuleEngine/BusinessRuleEngineProject/OrderProcessService/OrderProcessFactoryServiceTest.java
@@ -0,0 +1,24 @@
+package com.BusinessRuleEngine.BusinessRuleEngineProject.OrderProcessService;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OrderProcessFactoryServiceTest {
+
+    OrderProcessFactoryService service;
+    @Before
+    public void setUp() throws Exception{
+        this.service = new OrderProcessFactoryService();
+    }
+
+    @Test
+    public void testOrderForPhysicalProduct(){
+        int payment=100, orderId=1;
+        String expectedResponse = "Packing Slip generated.";
+        String response = service.processOrder(payment,orderId);
+        Assert.assertEquals(expectedResponse,response);
+    }
+}
